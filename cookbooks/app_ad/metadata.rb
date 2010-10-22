6ace644571065e293027e5589b060b3697d460d3
Raphael Simon <raphael@rightscale.com>
2010-10-22 12:13:22 -0700

    Change scope of attributes

--- cookbooks/app_ad/metadata.rb
+++ cookbooks/app_ad/metadata.rb
@@ -4,19 +4,19 @@ version          "0.0.1"
 recipe "app_ad::add_user", "Creates a new Active Directory user account, must be run on Domain Controller"
 recipe "app_ad::join", "Creates a new Active Directory user account, can be run anywhere"
 
-attribute "domain",
+attribute "ad/domain",
   :display_name => "Domain name",
   :description  => "Active Directory domain name",
   :recipes      => ["app_ad::add_user", "app_ad::join"],
   :required     => "required"
 
-attribute "new_username",
+attribute "ad/new_username",
   :display_name => "New Active Directory domain account user",
   :description  => "New Domain user name",
   :recipes      => ["app_ad::add_user", "app_ad::join"],
   :required     => "required"
 
-attribute "new_password",
+attribute "ad/new_password",
   :display_name => "New Active Directory domain account password",
   :description  => "New Domain user password",
   :recipes      => ["app_ad::add_user", "app_ad::join"],
--- cookbooks/app_ad/recipes/add_user.rb
+++ cookbooks/app_ad/recipes/add_user.rb
@@ -1,7 +1,7 @@
 # app_ad::add_user
 app_ad_active_directory do
-  domain   @node[:app_ad][:domain]
-  username @node[:app_ad][:new_username]
-  password @node[:app_ad][:new_password]
+  domain   @node[:ad][:domain]
+  username @node[:ad][:new_username]
+  password @node[:ad][:new_password]
   action :add_user
 end

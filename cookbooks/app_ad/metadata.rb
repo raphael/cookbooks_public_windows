b84cd5e425181b435dbe8c5715cac671b406d158
Raphael Simon <raphael@rightscale.com>
2010-10-22 11:46:29 -0700

    Change scope of attributes

--- cookbooks/app_ad/metadata.rb
+++ cookbooks/app_ad/metadata.rb
@@ -4,19 +4,19 @@ version          "0.0.1"
 recipe "app_ad::add_user", "Creates a new Active Directory user account, must be run on Domain Controller"
 recipe "app_ad::join", "Creates a new Active Directory user account, can be run anywhere"
 
-attribute "ad/domain",
+attribute "domain",
   :display_name => "Domain name",
   :description  => "Active Directory domain name",
   :recipes      => ["app_ad::add_user", "app_ad::join"],
   :required     => "required"
 
-attribute "ad/new_username",
+attribute "new_username",
   :display_name => "New Active Directory domain account user",
   :description  => "New Domain user name",
   :recipes      => ["app_ad::add_user", "app_ad::join"],
   :required     => "required"
 
-attribute "ad/new_password",
+attribute "new_password",
   :display_name => "New Active Directory domain account password",
   :description  => "New Domain user password",
   :recipes      => ["app_ad::add_user", "app_ad::join"],

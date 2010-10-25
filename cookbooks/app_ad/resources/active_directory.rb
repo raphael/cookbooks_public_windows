d4d3d7c118c61411eb6f86cf578373634d16f600
Raphael Simon <raphael@rightscale.com>
2010-10-25 16:16:42 -0700

    Change scope of attributes

--- cookbooks/app_ad/resources/active_directory.rb
+++ cookbooks/app_ad/resources/active_directory.rb
@@ -1,6 +1,6 @@
 actions :add_user
 
-attribute :domain,       :kind_of => [ String ], :regex => /^[a-z0-9][a-z0-9-\.]*$/i, :required => true, :name_attribute => true
+attribute :domain,       :kind_of => [ String ], :regex => /^[a-z0-9][a-z0-9\-\.]*$/i, :required => true, :name_attribute => true
 attribute :new_username, :kind_of => [ String ]
 attribute :new_password, :kind_of => [ String ]
 

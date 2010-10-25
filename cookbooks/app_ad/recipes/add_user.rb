e7f143716e9257f1b6492b44fe2208b070eb9d76
Raphael Simon <raphael@rightscale.com>
2010-10-25 12:53:10 -0700

    Change scope of attributes

--- cookbooks/app_ad/recipes/add_user.rb
+++ cookbooks/app_ad/recipes/add_user.rb
@@ -1,8 +1,13 @@
 # app_ad::add_user
-log @node.inspect
-app_ad_active_directory do
-  domain   @node['ad']#['domain']
-  username @node['ad']#['new_username']
-  password @node['ad']#['new_password']
-  action :add_user
+log "\n\n" + "-" * 80 + "\n"
+@node.each do |k, v|
+  log " " * 10 + "-" * 50
+  log "#{k}: #{v.inspect}"
 end
+#app_ad_active_directory do
+  #domain   @node['ad']['domain']
+  #username @node['ad']['new_username']
+  #password @node['ad']['new_password']
+  #action :add_user
+#end
+log "\n" + "-" * 80 + "\n\n"

4c4ee686e4919175ae9f748c7fe139fbf523fd98
Raphael Simon <raphael@rightscale.com>
2010-10-26 15:29:19 -0700

    Change scope of attributes

--- cookbooks/app_ad/recipes/join.rb
+++ cookbooks/app_ad/recipes/join.rb
@@ -1,13 +1,15 @@
 # app_ad::join
 
 # 1. Retrieve inputs
-domain   = node['ad']['domain']
-username = node['ad']['new_username']
-password = node['ad']['new_password']
+domain   = node[:ad][:domain]
+username = node[:ad][:new_username]
+password = node[:ad][:new_password]
 
 # 2. Send remote recipe
 remote_recipe 'join domain' do
   recipe 'app_ad::add_user'
-  attributes :domain => domain, :new_username => username, :new_password => password
+  attributes { :ad => { :domain       => domain, 
+                        :new_username => username,
+                        :new_password => password } }
   recipients_tags 'provides:ad_role=controller'
 end

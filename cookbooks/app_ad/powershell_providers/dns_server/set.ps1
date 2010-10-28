91e85d52cbda4cea7c1eec9a9d6f35ae5bac49ba
Raphael Simon <raphael@rightscale.com>
2010-10-28 10:00:13 -0700

    Latest changes

--- cookbooks/app_ad/powershell_providers/dns_server/set.ps1
+++ cookbooks/app_ad/powershell_providers/dns_server/set.ps1
@@ -1,10 +1,9 @@
 $name = Get-ChefNode ad, domain
 $ip = [System.Net.Dns]::GetHostByName($name).AddressList[0].IPAddressToString
-Write-Host Now using $ip for DNS resolution
-$NICs = Get-WmiObject '
-  -Class Win32_NetworkAdapterConfiguration '
-  -ComputerName $env:ComputerName '
-  -Filter "IPEnabled=TRUE"
+
+Write-Host "Now using $ip for DNS resolution"
+
+$NICs = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $env:ComputerName -Filter "IPEnabled=TRUE"
 
 foreach($NIC in $NICs) {
     $NIC.SetDNSServerSearchOrder($ip)

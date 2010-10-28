af332480404015cd0763915fa259ef77c8aefaff
Raphael Simon <raphael@rightscale.com>
2010-10-28 10:25:12 -0700

    Latest changes

--- cookbooks/app_ad/powershell_providers/dns_server/set.ps1
+++ cookbooks/app_ad/powershell_providers/dns_server/set.ps1
@@ -1,14 +1,10 @@
 $Domain = Get-ChefNode ad, domain
-Write-Host "Using domain $Domain as DNS"
-
 $Ip = [System.Net.Dns]::GetHostByName($Domain).AddressList[0].IPAddressToString
 Write-Host "Now using $Ip for DNS resolution"
 
 $NICs = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $env:ComputerName -Filter "IPEnabled=TRUE"
-
 foreach($NIC in $NICs) {
-    $NIC.SetDNSServerSearchOrder($Ip)
-    $NIC.SetDynamicDNSRegistration("TRUE")
-  }
+  $NIC.SetDNSServerSearchOrder($Ip)
+  $NIC.SetDynamicDNSRegistration("TRUE")
 }
 

b26f35ad3eb6e1736a4c6920246be3b1a23c20b9
Raphael Simon <raphael@rightscale.com>
2010-10-28 09:53:14 -0700

    Latest changes

--- cookbooks/app_ad/powershell_providers/dns_server/set.ps1
+++ cookbooks/app_ad/powershell_providers/dns_server/set.ps1
@@ -1,6 +1,6 @@
 $name = Get-ChefNode ad, domain
 $ip = [System.Net.Dns]::GetHostByName($name).AddressList[0].IPAddressToString
-
+Write-Host Now using $ip for DNS resolution
 $NICs = Get-WmiObject '
   -Class Win32_NetworkAdapterConfiguration '
   -ComputerName $env:ComputerName '

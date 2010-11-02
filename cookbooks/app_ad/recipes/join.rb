6c11dc9d2b89f3ddbea5ee094a717e4d7c42263b
Raphael Simon <raphael@rightscale.com>
2010-11-01 17:01:54 -0700

    Latest changes

--- cookbooks/app_ad/recipes/join.rb
+++ cookbooks/app_ad/recipes/join.rb
@@ -29,8 +29,8 @@ app_ad_dns_server domain do
 end
 
 ## 4. Join domain
-#app_ad_active_directory domain do
-  #admin_username admin_username
-  #admin_password admin_password
-  #action :join
-#end
+app_ad_active_directory domain do
+  admin_username admin_username
+  admin_password admin_password
+  action :join
+end

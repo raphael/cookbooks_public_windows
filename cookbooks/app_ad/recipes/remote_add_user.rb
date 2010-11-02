3b0666fcf015c22e361736347597568c042e50c3
Raphael Simon <raphael@rightscale.com>
2010-11-02 11:20:20 -0700

    Latest changes

---
+++ cookbooks/app_ad/recipes/remote_add_user.rb
@@ -0,0 +1,20 @@
+# app_ad::remote_add_user
+
+# 1. Retrieve inputs
+domain   = node[:ad][:domain]
+username = node[:ad][:user_username]
+password = node[:ad][:user_password]
+
+log "domain #{domain}"
+log "username #{username}"
+
+# 2. Send remote recipe to add user
+remote_recipe 'add user' do
+  recipe 'app_ad::add_user'
+  attributes :remote_recipe => {
+               :ad => { :domain        => domain, 
+                        :user_username => username,
+                        :user_password => password } }
+  recipients_tags 'provides:ad_role=controller'
+end
+

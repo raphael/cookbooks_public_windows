53b3b8b68e830c123f9baca60aeb9aa274032554
Raphael Simon <raphael@rightscale.com>
2010-11-02 10:30:43 -0700

    Latest changes

--- cookbooks/app_ad/powershell_providers/active_directory/join.ps1
+++ cookbooks/app_ad/powershell_providers/active_directory/join.ps1
@@ -2,7 +2,7 @@
 $Domain   = Get-ChefNode ad, domain
 $UserName = Get-ChefNode ad, admin_username
 $Password = Get-ChefNode ad, admin_password 
-ConvertTo-SecureString $Password -AsPlainText -Force
+$Password = ConvertTo-SecureString $Password -AsPlainText -Force
 
 $UserName = "$Domain\$UserName"
 $Cred = New-Object System.Management.Automation.PSCredential $UserName, $Password
